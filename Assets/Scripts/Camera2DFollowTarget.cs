5b1f2773048936ee537c0f9877da13cb8a4b8954
North <NorthRiver@laposte.net>
2018-10-16 12:16:51 +0100

    [FEATURE] Camera follow + Player basic controll

---
+++ Assets/Scripts/Camera2DFollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2DFollowTarget : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField] private float smoothSpeed = 10f;
+    [SerializeField] private Vector3 offset;
+
+    private void LateUpdate()
+    {
+       Vector3 desiredPosition = target.position + offset;
+       Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+       transform.position = smoothedPosition;
+    }
+
+
+}
---
+++ Assets/Scripts/CharacterControllerSnail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerSnail : MonoBehaviour {
+    
+	public float movementSpeed = 3f;
+    public float rotationSpeed = 150f;
+
+    // Update is called once per frame
+    void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
