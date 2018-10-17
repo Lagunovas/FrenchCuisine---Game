53c93a2ff3b0e804e33de8120aa8e76809a0749c
North <NorthRiver@laposte.net>
2018-10-17 16:51:23 +0100

    [Feature] Trail shrinking

---
+++ Assets/Scripts/TrailSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailSystem : MonoBehaviour {
+
+    [SerializeField] private Transform generator;
+    [SerializeField] private float delay;
+    [SerializeField] private GameObject trailPrefab;
+
+    private float timeSinceLastTrail = 0;
+
+    private void Update()
+    {
+        timeSinceLastTrail += Time.deltaTime;
+        if (delay <= timeSinceLastTrail)
+        {
+            timeSinceLastTrail = 0;
+            Instantiate(trailPrefab, generator.position, generator.rotation);
+        }
+    }
+
+
+}
