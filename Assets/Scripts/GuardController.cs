221bf4b2e7d22f3657bf9481455c56fbb722b52e
Edgaras Lagunovas <elag@live.ie>
2018-10-23 01:55:45 +0100

    Guard - Nav Mesh pathfinding

--- Assets/Scripts/GuardController.cs
+++ Assets/Scripts/GuardController.cs
@@ -1,32 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardController : MonoBehaviour {
 
-	private float timer;
-	private Rigidbody rb;
-	private float startingRotation;
+	[SerializeField] private Transform target;
+	private NavMeshAgent agent;
 
-	void Start() {
-		rb = GetComponent<Rigidbody>();
-		startingRotation = rb.rotation.eulerAngles.y;
+	private void Start() {
+		agent = GetComponent<NavMeshAgent>();
 	}
 
-	private void FixedUpdate() {
-		timer += Time.fixedDeltaTime;
-
-		if (timer >= 5f) {
-			var targetRotation = (startingRotation + 90f) % 360f;
-			var nextRotation = Mathf.MoveTowardsAngle(rb.rotation.eulerAngles.y, targetRotation, 90f * Time.fixedDeltaTime);
-			rb.MoveRotation(Quaternion.Euler(new Vector3(0, nextRotation, 0)));
-
-			if (nextRotation == targetRotation) {
-				timer = 0;
-				startingRotation = startingRotation + 90f;
-			}
-			return;
-		}
-
-		rb.MovePosition(transform.position + (transform.forward * Time.fixedDeltaTime));
+	private void Update() {
+		agent.SetDestination(target.position);
 	}
 
 }
