5aa448337145c0c11070f45bfdb833dea27f5a56
North <NorthRiver@laposte.net>
2018-10-25 11:48:43 +0100

    [Scene] Menu Start

---
+++ Assets/Scripts/SnailMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailMenu : MonoBehaviour {
+
+    public float movementSpeed = 3f;
+
+    public Animator animator;
+    private bool available = true;
+
+    void Update()
+    {
+
+
+                animator.SetInteger("state", 1);
+                normalMovement();
+
+    }
+
+    void normalMovement()
+    {
+        
+        var z =  Time.deltaTime * movementSpeed;
+
+        transform.Translate(0, 0, z);
+        if (transform.position.z >= 30)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -30) ;
+        }
+    }
+}
