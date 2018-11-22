8ca8ab6314f23ca3f3bca1d530f8e476e34ef82c
Pierre <pierre.galus@live.fr>
2018-11-22 10:54:14 +0100

    Sort name levels

---
+++ Assets/Scripts/exitBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitBehavior : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(4);
+
+        }
+    }
+}
